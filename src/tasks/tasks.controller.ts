6aa6318ab8e6d38eb1a27b403081a01db19bc935
Apurva Tripathi <apurva@hyperverge.co>
2020-07-11 11:52:42 +0530

    Create typeorm config, setup pg server

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -9,18 +9,20 @@ import {
   Patch,
   UsePipes,
   ValidationPipe,
+  Query,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './tasks.model';
 import { CreateTaskDto } from './dto/create-task-dto';
 import { TaskStatusValidatorPipe } from './pipes/task-status-validation.pipe';
+import { GetTasksFilterDto } from './dto/get-task-filter-dto';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  getAllTasks(): Task[] {
+  getAllTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Task[] {
     return this.tasksService.getAllTasks();
   }
 
